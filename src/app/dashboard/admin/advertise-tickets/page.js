8c15916216fe30b7927c969c1a3a64e22aa5a2d0
Abida-jannat <abidajannatchy03@gmail.com>
2026-09-06 02:19:59 +0600

    change backend link

--- src/app/dashboard/admin/advertise-tickets/page.js
+++ src/app/dashboard/admin/advertise-tickets/page.js
@@ -2,8 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:5000";
-
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 export default function AdvertiseTicketsPage() {
   const [tickets, setTickets] = useState([]);
   const [loading, setLoading] = useState(true);
